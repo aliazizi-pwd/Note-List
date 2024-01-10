4e062feeb081d6d8825fa6a1e33b0e9b2f13d6b5
Ali Azizi <aliazizi.4973@gmail.com>
2024-01-10 22:41:55 +0330

    Codeing part Change Theme Note App and load Web Page

--- source/javaScript/app.js
+++ source/javaScript/app.js
@@ -13,6 +13,7 @@ const modal = $.querySelector(".modal-main");
 const nameModal = $.querySelector(".name-modal");
 const titleModal = $.querySelector(".title-modal");
 const noteApp = $.querySelector(".note-app");
+const body = $.querySelector("body");
 // -> select button click :|
 const btnAdd = $.querySelector(".btn-Add");
 const btnClearAll = $.querySelector(".btn-clearAll");
@@ -25,16 +26,36 @@ const btnChangeTheme = $.querySelector(".changeTheme");
 // -> The code section of the function to change the theme
 function changeThemeHandler () {
     if (theme === "Light") {
-        $.body.classList.replace("bg-body-tertiary", "bg-darkMode");
-        $.body.classList.replace("text-light", "text-dark");
-        noteApp.classList.replace("text-dark", "text-light");
+        applyDarkThemeHandler(body,noteApp);
         theme = "Dark";
     } else {
-        $.body.classList.replace("bg-darkMode","bg-body-tertiary");
-        $.body.classList.replace("text-dark", "text-light");
-        noteApp.classList.replace("text-light", "text-dark");
+        applyLightThemeHandler(body,noteApp);
         theme = "Light";
     }
+
+    // set the theme active to local storage
+    localStorage.setItem("themeNoteApp", theme);
+}
+
+
+// -> apply Dark Mode 
+function applyDarkThemeHandler (body,noteApp) {
+    body.classList.replace("bg-body-tertiary","bg-darkMode");
+    body.classList.replace("text-light","text-dark");
+    noteApp.classList.replace("text-dark","text-light");
+}
+
+// -> apply Light Mode
+function applyLightThemeHandler (body,noteApp) {
+    body.classList.replace("bg-darkMode","bg-body-tertiary");
+    body.classList.replace("text-dark","text-light");
+    noteApp.classList.replace("text-light","text-dark");
+}
+
+// -> The code section of the function load web note app page
+function loadNoteAppHandler () {
+    let receiveThemeLocalStorage = localStorage.getItem("themeNoteApp");
+    receiveThemeLocalStorage === "Dark" ? changeThemeHandler() : null;
 }
 
 
@@ -42,3 +63,5 @@ function changeThemeHandler () {
 
 // -> set click event Listener for Click Button's Application
 btnChangeTheme.addEventListener("click",changeThemeHandler);
+// -> set click event Listener for Window and Document Self
+window.addEventListener("load",loadNoteAppHandler);

3c087fc90bd0d722a766788d43cef9fdbb9f9e51
Ali Azizi <aliazizi.4973@gmail.com>
2024-01-10 21:36:45 +0330

    Codeing part select element HTML

--- source/javaScript/app.js
+++ source/javaScript/app.js
@@ -2,3 +2,19 @@
 // -> select element HTML let <-
 let $ = document;
 // -> select element HTML const <-
+const innerTitle = $.querySelector(".input-title");
+const innerDate = $.querySelector(".input-date");
+const innerContent = $.querySelector(".input-content");
+// -> select element HTML Main <-
+const dataBaseNote = $.querySelector(".box-note-items");
+const countNote = $.querySelector(".count-note");
+const modal = $.querySelector(".modal-main");
+const nameModal = $.querySelector(".name-modal");
+const titleModal = $.querySelector(".title-modal");
+// -> select button click :|
+const btnAdd = $.querySelector(".btn-Add");
+const btnClearAll = $.querySelector(".btn-clearAll");
+const btnFilterAll = $.querySelector(".All");
+const btnFilterComplete = $.querySelector(".Completed");
+const btnFilterUnComplete = $.querySelector(".unCompleted");
+const btnChangeTheme = $.querySelector(".changeTheme");
